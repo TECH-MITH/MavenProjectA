bb80bbf7f88b98db546de36cdd5554f96ec22533
Mithilesh Kumar Chaurasiya <71425926+TECH-MITH@users.noreply.github.com>
2020-09-17 22:16:04 +0530

    Add files via upload

---
+++ MavenProjectA/src/test/java/com/codebind/ProblemTest.java
@@ -0,0 +1,19 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	private void fail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
